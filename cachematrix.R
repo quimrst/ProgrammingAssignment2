28763e829cd004179cdad5cd265dca33b43c4484
quimrst <82025788+quimrst@users.noreply.github.com>
2021-04-06 18:09:36 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,27 @@
 makeCacheMatrix <- function(x = matrix()) {
   k <- NULL
-  set <- function(l) {
+  create <- function(l) {
     r <<- l
     k <<- NULL
   }
-  get <- function() r
-  setsolve <- function(solve) k <<- solve
-  getsolve <- function() k
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+  obtain <- function() r
+  createinverse <- function(inverse) k <<- inverse
+  obtaininverse <- function() k
+  list(create = create, obtain = obtain,
+       createinverse = createinverse,
+       obtaininverse = obtaininverse)
 }
 
 ## Codes above are for creating inverse matrix
 
 cacheSolve <- function(r, ...) {
-  f <- r$getsolve()
+  f <- r$obtaininverse()
   if(!is.null(k)) {
     message("inversed matrix in progress")
     return(i)
   }
-  data <- r$get()
+  data <- r$obtain()
   k <- solve(data, ...)
-  r$setsolve(k)
+  r$setinverse(k)
   s
 }
